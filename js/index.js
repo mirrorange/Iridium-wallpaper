06527a4fdd53bc0ada9d0fdf67111e3ea403e3cc
orange <orange@icedeer.net>
2021-12-26 15:16:21 +0800

    修复js加载顺序的bug

--- js/index.js
+++ js/index.js
@@ -26,6 +26,35 @@ function setbackground(backgroundurl) {
     }
 }
 
+function seriesLoadScripts(scripts) {
+    if (typeof (scripts) !== 'object') {
+        var scripts = [scripts];
+    }
+    var BODY = document.getElementsByTagName('body')[0] || document.documentElement;
+    var s = [];
+    var last = scripts.length - 1;
+    //递归
+    var recursiveLoad = function (i) {
+        s[i] = document.createElement('script');
+        s[i].setAttribute('type', 'text/javascript');
+        // Attach handlers for all browsers
+        // 异步
+        s[i].onload = s[i].onreadystatechange = function () {
+            if (!/*@cc_on!@*/0 || this.readyState === 'loaded' || this.readyState === 'complete') {
+                this.onload = this.onreadystatechange = null;
+                this.parentNode.removeChild(this);
+                if (i !== last) {
+                    recursiveLoad(i + 1);
+                }
+            }
+        }
+        // 同步
+        s[i].setAttribute('src', scripts[i]);
+        BODY.appendChild(s[i]);
+    };
+    recursiveLoad(0);
+}
+
 function loadplugin(plugin) {
     plugin["css"].forEach(element => {
         cssele = document.createElement("link");
@@ -33,18 +62,14 @@ function loadplugin(plugin) {
         cssele.setAttribute("href", element);
         document.getElementsByTagName("head")[0].appendChild(cssele)
     });
-    plugin["js"].forEach(element => {
-        jsele = document.createElement("script");
-        jsele.setAttribute("src", element);
-        document.getElementsByTagName("body")[0].appendChild(jsele)
-    });
+    seriesLoadScripts(plugin["js"]);
 }
 
 function loadapp(app) {
     var appli = document.createElement("li");
     appli.className = "mdl-menu__item";
     appli.innerText = app["name"];
-    appli.setAttribute("appurl",app["url"]);
+    appli.setAttribute("appurl", app["url"]);
     appli.onclick = openapp;
     document.getElementById("applist").appendChild(appli);
 }
@@ -56,14 +81,13 @@ function openapp() {
     var appiframe = document.createElement("iframe");
     appiframe.id = "appiframe";
     appiframe.allowtransparency = true;
-    appiframe.setAttribute("src",this.getAttribute("appurl"));
+    appiframe.setAttribute("src", this.getAttribute("appurl"));
     var body = document.getElementsByTagName("body")[0];
     body.appendChild(coverdiv);
     body.appendChild(appiframe);
 }
 
-function closeapp()
-{
+function closeapp() {
     document.getElementById("appiframe").remove();
     document.getElementById("coverdiv").remove();
 }
